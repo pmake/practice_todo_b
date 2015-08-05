c13829d2fa07eda13f6bde47c5170be8b220824f
pmake <unique_ntsn@hotmail.com>
2015-08-05 09:33:14 +0000

    foutrh commit

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-function add(){
-    var cont = document.createElement("p");
-    document.body.appendChild(cont);
-     cont.innerHTML = document.getElementById("context").value;
-}
+var todo = (function(){
+    var todos = [],finTodos = [];
+    // for reducing DOM reading.
+    var todoDiv = document.getElementById("todoArea"),
+    finDiv = document.getElementById("finArea");
+    
+    // functions
+    function load(){
+        todos = localStorage.todos;
+        finTodos = localStorage.finTodos;
+    }
+    
+    function add(){
+        creEle("P",document.getElementById("inputData").value,todoDiv);
+        document.getElementById("inputData").value ="";
+    }
+    
+    function modify(){
+        
+    }
+    function save(){
+     
+    }
+    function update(){
+        
+        
+    }
+    function creEle(element,text,dest){
+        var node = document.createElement(element),
+        textNode = document.createTextNode(text);
+        node.appendChild(textNode);
+        dest.appendChild(node);
+    }
+    return {load:load,add:add,modify:modify,save:save,update:update};
+}());
 
-document.getElementById("submit").addEventListener('click',add);
+todo.load();
+
+// Add listeners.
+document.getElementById("submit").addEventListener('click',todo.add);
